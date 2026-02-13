4f94084bf510f79bfdbf4b2c866d0cd9b3c8e67b
Rahul rc <rc7926422@gmail.com>
2026-02-13 21:30:27 +0530

    Add files via upload

---
+++ Difficulty Basic/Maximum product of two numbers.java
@@ -0,0 +1,17 @@
+class Solution {
+    public static int maxProduct(int[] arr) {
+        int firstMax = Integer.MIN_VALUE;
+        int secondMax = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > firstMax) {
+                secondMax = firstMax;
+                firstMax = num;
+            } else if (num > secondMax) {
+                secondMax = num;
+            }
+        }
+
+        return firstMax * secondMax;
+    }
+}
