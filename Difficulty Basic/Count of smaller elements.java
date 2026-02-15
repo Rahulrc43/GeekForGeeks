c18880ace16e501e81e83ef5c7e60ac45e4c1620
Rahul rc <rc7926422@gmail.com>
2026-02-15 21:34:59 +0530

    Add files via upload

---
+++ Difficulty Basic/Count of smaller elements.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int countOfElements(int x, List<Integer> arr) {
+        int count = 0;
+        for (int num : arr) {
+            if (num <= x) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
