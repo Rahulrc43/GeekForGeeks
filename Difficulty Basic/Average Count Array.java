3c348c4d4678d4ddb724b2801db05ce3077414e1
Rahul rc <rc7926422@gmail.com>
2026-03-03 21:53:12 +0530

    Add files via upload

---
+++ Difficulty Basic/Average Count Array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public ArrayList<Integer> countArray(int[] arr, int x) {
+        int n = arr.length;
+        ArrayList<Integer> result = new ArrayList<>(n);
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        for (int num : arr) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
+        for (int i = 0; i < n; i++) {
+            int avg = (arr[i] + x) / 2;
+            result.add(freq.getOrDefault(avg, 0));
+        }
+        return result;
+    }
+}
